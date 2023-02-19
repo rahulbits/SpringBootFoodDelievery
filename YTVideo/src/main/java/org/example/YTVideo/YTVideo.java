bce5d1dc3df4dfab1ffb8dad46e3532db7c9cf4d
Rahul Ranjan <rk527585@gmail.com>
2023-02-19 17:01:50 +0530

    Removed Some un-necessary Imports

--- YTVideo/src/main/java/org/example/YTVideo/YTVideo.java
+++ YTVideo/src/main/java/org/example/YTVideo/YTVideo.java
@@ -1,16 +1,10 @@
 package org.example.YTVideo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-
-import javax.persistence.Entity;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableJpaRepositories
